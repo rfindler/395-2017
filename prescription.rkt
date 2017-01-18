70686fef99c810d2c03619f94e15ec41bbdc1de3
fluffywaffles <skorlir@gmail.com>
2017-01-18 12:39:47 -0600

    Some tweaks, but mostly tests for pharmacist actions.

--- prescription.rkt
+++ prescription.rkt
@@ -13,7 +13,7 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; Patient.
-(struct patient (first-name last-name))
+(struct patient (first-name last-name)) ;; currently-taking))
 (define patient/c
   (struct/c patient
             string?   ; first-name
@@ -70,16 +70,16 @@
             addtl/c))         ; additional description
 
 ;; Script-reason.
-(define reason/c (or/c string? null?))
+(define optional-string/c (or/c string? null?))
 
 ;; Script.
-(struct script (patient rx sig desc))
+(struct script (patient rx sig reason))
 (define script/c
   (struct/c script
-            patient/c  ; patient
-            rx/c       ; rx
-            sig/c      ; sig
-            reason/c)) ; reason
+            patient/c           ; patient
+            rx/c                ; rx
+            sig/c               ; sig
+            optional-string/c)) ; reason
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
@@ -112,8 +112,12 @@
 ;; • sig-addtl-instructions interpreter that translates "po" to "by mouth," "qhs" to "before
 ;;     bed," etc.
 ;; • generate-label: take a script and generate a reasonable label for the medication bottle.
+;;     --- Note that labels actually say things like "Omeprazole... Substituted For: Prilosec."
 ;; • better script validation - it should not be possible for instance to have
-;;     (redundant-count "••" 4).
+;;     (redundant-count "••" 4), or to write an Rx for a nonexistent drug, etc.
+;; • drug medium - capsule, tablet, liquid, etc. This is also something that usually shows up on a
+;;     label, but which we don't really know if/when a doctor specifies. A pharmacist however has to
+;;     know and/or decide.
 ;;
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -134,13 +138,13 @@
 ;; translate-script :: script/c -> script/c
 ;;
 ;; roughly:
-;; (letrec ((transformations (valid-fill-options-for script))
+;; (letrec ((transformations (valid-fill-methods-for script))
 ;;          (transformation (magically-choose-one transformations)))
 ;;    (when (not (equal? (transformation script) script))
 ;;      (alert-doctor 'substitution-made transformation))
 ;;    (fill (transformation script)))
 ;;
-;; magically-choose-one and valid-fill-options-for have signatures something like...
+;; magically-choose-one and valid-fill-methods-for have signatures something like...
 ;; fill-options :: script/c -> (listof script-translation/c)
 ;; magically-choose-one :: (listof script-translation/c) -> script-translation/c
 ;;
@@ -190,7 +194,7 @@
       available-rx)))
 
 ;; Find all valid script-translation/c for a given script using only available-rx.
-(define/contract (valid-fill-options-for script available-rx)
+(define/contract (valid-fill-methods-for script available-rx)
   (-> script/c (listof rx/c) (listof script-translation/c))
   ;; NOTE: this is where the mdb idea becomes really appealing again. Even just using
   ;;   a SQL query would be cleaner, more flexible, than something bespoke like this.
@@ -205,10 +209,14 @@
 ;;
 ;; Pharmacist translation tests.
 ;;
+;; Translation really feels like it should be... more of a process that looks like "here's an ORM
+;; query from the doctor that describes the drug he wants prescribed, run the query against your
+;; available Rx."
+;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (check-equal?
- (valid-fill-options-for example-script (list (script-rx example-script)))
+ (valid-fill-methods-for example-script (list (script-rx example-script)))
  `((,exact-rx ,example-script ,(script-rx example-script))))
 
 (define prilosec
@@ -227,28 +235,29 @@
   (list prilosec minocycline prescription-strength-tylenol probably-deadly-tylenol))
 
 (check-equal?
-  (valid-fill-options-for example-script some-rx)
+  (valid-fill-methods-for example-script some-rx)
   '())
 
 (check-equal?
-  (valid-fill-options-for example-script (reverse (cons (script-rx example-script) some-rx)))
+  (valid-fill-methods-for example-script (reverse (cons (script-rx example-script) some-rx)))
   `((,exact-rx ,example-script ,(script-rx example-script))))
 
 ;; FIXME: this will currently fail.
 (check-equal?
-  (valid-fill-options-for example-script (cons not-quite-same-zestril some-rx))
+  (valid-fill-methods-for example-script (cons not-quite-same-zestril some-rx))
   `((,substitute-rx ,example-script ,not-quite-same-zestril)))
 
 (define tylenol-script
   (script
    (patient "Mark" "The Zuck")
    (rx (drug "Acetominophen") (dosage 500 "mg") (doses 1))
-   ;; NOTE: the unicode codepoint for • is 2022.
+   ;; NOTE the unicode codepoint for • is 2022.
    (sig (redundant-count "•" 1) (addtl '()))
    "for your neighbor's annoying dog"))
 
 (check-equal?
-  (valid-fill-options-for tylenol-script some-rx)
+  (valid-fill-methods-for tylenol-script some-rx)
+  ;; BUG probably-deadly-tylenol will have the wrong "doses" because it's stock, not dispensed.
   `((,exact-rx ,tylenol-script ,probably-deadly-tylenol)))
 
 (define unfillably-large-script
@@ -262,6 +271,170 @@
 ;;   Stuff like that. We should probably have a struct we can modify as-we-go that builds up these
 ;;   sorts of things as they occur.
 (check-equal?
-  (valid-fill-options-for unfillably-large-script some-rx)
+  (valid-fill-methods-for unfillably-large-script some-rx)
   '())
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; Pharmacists and doctors need to be modeled, and they need to be able to converse.
+;; There should also be an actual patient model, instead of just a pair of names.
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Doctor writes a script.
+(define (doctor/write-script patient)
+  ;; TODO
+  (script
+    patient
+    (rx (drug "Omeprazole") (dosage 20 "mg") (doses 10))
+    (sig (redundant-count "••" 2) (addtl '("po")))
+    "for gastroesophageal reflux disorder"))
+
+;; Script signing.
+(struct signed (script signature))
+(define signed/c
+  (struct/c signed
+            script/c            ; script
+            optional-string/c)) ; signature
+  #| (struct/c signed |#
+  #|           ;; FIXME do I really have to copy this? |#
+  #|           patient/c           ; patient |#
+  #|           rx/c                ; rx |#
+  #|           sig/c               ; sig |#
+  #|           optional-string/c   ; reason |#
+  #|           ; |#
+  #|           optional-string/c)) ; signature |#
+
+;; Doctor signs the script.
+(define (doctor/sign-script script name)
+  (signed script name))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; Pharmacist uses the query to dispense, confirms.
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Verify query is signed.
+(define (pharmacist/verify some-script)
+  (if (not (null? (signed-signature some-script)))
+    (signed-script some-script)
+    ;; QUESTION should this be an error?
+    (error 'pharmacist/verify "The script is not signed.")))
+
+;; script -> adjusted, substituted, disambiguated (if necessary) script.
+(define (pharmacist/adjust original-script actual-fill-method)
+  ;; TODO actually adjust the script, based on results of pharmacist/find.
+  original-script)
+
+;; adjusted-script -> actual-medication
+;; TODO
+;; stopgap: script -> rx-with-label
+;; Need a quick struct for adding a label to rx.
+(struct rx-with-label (rx label))
+(define rx-with-label/c
+  (struct/c rx-with-label
+            rx/c      ; actual rx
+            string?)) ; label
+;; Now actually take a script and give an rx-with-label back.
+(define (pharmacist/fill script stock)
+  ;; TODO better sig-to-string.
+  (let ([sig-to-string
+          (lambda (sig)
+            "#<sig>")])
+    (let ([fill-options (valid-fill-methods-for script stock)])
+      (when (empty? fill-options)
+        (error 'pharmacist/fill "Cannot fill script. Please come again later.")))
+    (letrec ([fill-method (first (valid-fill-methods-for script stock))]
+             [new-script (pharmacist/adjust script fill-method)])
+      ; TODO
+      ;(alert-doctor 'script-filled script fill-method)
+      ; and something like:
+      ;(save-prescription-record script new-script fill-date) ; gives an rxid
+      ;(add-to-shelf (rx-with-label ...))
+      (rx-with-label (script-rx new-script)
+                     (string-append
+                       (sig-to-string (script-sig new-script))
+                       "\n"
+                       (script-reason new-script))))))
+
+;; pharmacist -> patient -> patient (now with new medications).
+(define (pharmacist/dispense pharmacist patient)
+  ;; TODO something like...
+  ;(get-from-shelf (rx-for patient))
+  ;(struct-update patient [currently-taking (append (patient-currently-taking) new-stuff)])
+  ;(alert-doctor 'medication-dispensed patient new-stuff)
+  null)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; Tests. Any tests. Does anything work?
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Signing and verification.
+(check-equal?
+  (pharmacist/verify (doctor/sign-script example-script "John Hancock"))
+  example-script)
+
+(check-exn
+  exn:fail?
+  (lambda ()
+    (pharmacist/verify (signed example-script null))))
+
+;; Pharmacist fill.
+(check-exn
+  exn:fail?
+  (lambda ()
+    (pharmacist/fill example-script some-rx)))
+
+(check-equal?
+  (rx-with-label-rx (pharmacist/fill example-script (cons (script-rx example-script) some-rx)))
+  (script-rx example-script))
+
+(check-equal?
+  (rx-with-label-rx (pharmacist/fill tylenol-script some-rx))
+  (script-rx tylenol-script))
+
+(check-equal?
+  (rx-with-label-label (pharmacist/fill tylenol-script some-rx))
+  "#<sig>\nfor your neighbor's annoying dog")
+
+;; FIXME this will currently be an exn. Need to make valid-fill-methods-for return a valid fill
+;;   method when there's a way to fill the rx inexactly.
+(check-equal?
+  (rx-with-label-rx (pharmacist/fill example-script (cons not-quite-same-zestril some-rx)))
+  not-quite-same-zestril)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; Stuff that isn't figured out yet.
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define (script->query script)
+  ;; TODO make script more executable.
+  script)
+
+;; TODO find a conversion from script->actual-medication, finalize this model.
+(struct actual-medication rx (medium label rxid refills-allowed?))
+(define actual-medication/c
+  (struct/c actual-medication
+            ; FIXME do I really have to copy this?
+            drug/c     ; inherited...
+            dosage/c   ; inherited...
+            doses/c    ; inherited...
+            ;
+            any/c ;medium/c   ; medium - eg capsule
+            string?    ; label text
+            number?    ; prescription id (for looking up at the pharmacy)
+            boolean?)) ; refills allowed
+
+;;
+;; Pharmacist needs to be able to confirm translation and dispensal with doctor.
+;; Also needs to be able to return a new patient who has new drugs.
+;;
+;; And, at any point, the doctor should be able to (alert-pharmacist 'stop-dispense) or 'alter-script
+;; or what-have-you in case things change... These communications channels are undefined now.
+;;
 
