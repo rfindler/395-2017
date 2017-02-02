00c857998086444a2938fd0a264caa7121331954
Daniel Feltey <dfeltey@gmail.com>
2017-02-02 14:12:24 -0600

    Fix scrolling issues

--- task-gui.rkt
+++ task-gui.rkt
@@ -4,8 +4,8 @@
 
 (require racket/gui)
 
-;; build-task-gui : get-task-name get-task-text get-task-callback -> ((listof task?) -> void?)
-(define ((make-task-gui-builder task-name task-text task-callback)
+;; build-task-gui : get-task-name get-task-text resolve-task! -> ((listof task?) -> void?)
+(define ((make-task-gui-builder task-name task-text resolve-task)
          tasks
          #:width [width 500]
          #:height [height 500]
@@ -13,36 +13,38 @@
   (define the-frame
     (new frame% [label label] [width width] [height height]))
 
-  (define processed-tasks (process-task-list tasks task-name task-text task-callback))
+  (define the-panel
+    (new vertical-panel% [parent the-frame] [style '(auto-vscroll auto-hscroll)]))
+
+  (define processed-tasks (process-task-list tasks task-name task-text))
   (for ([(name task-details) (in-hash processed-tasks)])
     (define group-box
       (new group-box-panel%
            [label name]
-           [parent the-frame]
+           [parent the-panel]
            [alignment (list 'left 'top)]))
     (for ([task-detail (in-list task-details)])
-      (match-define (cons text callback) task-detail)
+      (match-define (cons text task) task-detail)
       (new button%
            [label text]
            [parent group-box]
            [callback (λ (btn evt)
                        ;; call the callback then remove the button
-                       ;; ASSUME no args to callback for now
-                       (callback)
+                       ;; ASSUME no additional args to resolve-task for now
+                       (resolve-task task)
                        (send group-box delete-child btn))])))
   (send the-frame show #t))
 
 
 ;; process-task-list : (listof task?) get-task-name get-task-text get-task-callback
 ;;                     -> (hash/c string? (cons/c string? procedure?))
-(define (process-task-list tasks task-name task-text task-callback)
+(define (process-task-list tasks task-name task-text)
   (for/fold ([task-hash (hash)])
             ([task (in-list tasks)])
     (define name (task-name task))
     (define text (task-text task))
-    (define callback (task-callback task))
     (hash-update task-hash
                  name
-                 (λ (lst) (cons (cons text callback) lst))
+                 (λ (lst) (cons (cons text task) lst))
                  null)))
   
--- tasks-start.rkt
+++ tasks-start.rkt
@@ -8,18 +8,19 @@
 
 ;; string string (any ... -> any) -> void
 (define (add-task! name text)
+  (define callback (λ () (printf "resolving task: ~a\n for patient: ~a\n\n" text name)))
   (set! tasks
-        (cons (task name text void) tasks)))
+        (cons (task name text callback) tasks)))
 
-;; task (listof any) -> void
-(define (resolve-task! task values)
+;; task any ... -> void
+(define (resolve-task! task . values)
   (set! tasks (remove task tasks))
   (apply (task-callback task) values))
 
 ;; the library
 
 (define (urgent-hypertension! patient when-creat when-neuro)
-  (add-task! patient (~a "order check creatinine" when-creat))
+  (add-task! patient (~a "order check creatinine " when-creat))
   (add-task! patient "check blood pressure")
   (add-task! patient "order check K+")
   (add-task! patient "current hypertension drug check")
@@ -35,9 +36,42 @@
 (add-task! "jones" "order 2g sodium diet")
 (add-task! "jones" "consult nephrology")
 
+;; A second patiend
+(urgent-hypertension! "smith" "tomorrow morning" "Stat")
+(add-task! "smith" "order CT angiogram of kidneys")
+(add-task! "smith" "review current literature on diagnosing hyperaldosteronism")
+(add-task! "smith" "order 2g sodium diet")
+(add-task! "smith" "consult nephrology")
+
+(urgent-hypertension! "foo" "tomorrow morning" "Stat")
+(add-task! "foo" "order CT angiogram of kidneys")
+(add-task! "foo" "review current literature on diagnosing hyperaldosteronism")
+(add-task! "foo" "order 2g sodium diet")
+(add-task! "foo" "consult nephrology")
+
+
+(urgent-hypertension! "bar" "tomorrow morning" "Stat")
+(add-task! "bar" "order CT angiogram of kidneys")
+(add-task! "bar" "review current literature on diagnosing hyperaldosteronism")
+(add-task! "bar" "order 2g sodium diet")
+(add-task! "bar" "consult nephrology")
+
+(urgent-hypertension! "baz" "tomorrow morning" "Stat")
+(add-task! "baz" "order CT angiogram of kidneys")
+(add-task! "baz" "review current literature on diagnosing hyperaldosteronism")
+(add-task! "baz" "order 2g sodium diet")
+(add-task! "baz" "consult nephrology")
+
 ;; the plan
 
 ;; before next class: add a gui thing
 ;; during next class: how to conceptualize and write loops and conditionals
+(require "task-gui.rkt")
 
-tasks
+;; setup gui-builder
+(define run-task-manager
+  (make-task-gui-builder task-name task-text resolve-task!))
+
+(run-task-manager tasks)
+
+;; Maybe there should be a separate list of patients to show patients with no available tasks
